1852831ba329c4d131f47ac5d1f4d81d74c3c609
raulportela <raul.p.p@hotmail.com>
2021-08-19 00:29:39 -0300

    Funcoes TS, tela de doacao

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { HTMLAttributes } from 'react';
+import { HTMLProps } from 'react';
 import ReactDOM from 'react-dom';
 
 function toggleMenu() {
@@ -12,6 +13,19 @@ function toggleMenu() {
     toggle.classList.toggle('active');
     navigation.classList.toggle('active');
     main.classList.toggle('active');
+}
+function onChangeQtd() {
+    var qtdM = document.getElementById("qtd-moedas")?.nodeValue;
+    var valorMoeda = document.getElementById("vlr-uni")?.nodeValue; //substituir esse treco, pelo comando real da moeda, inves de pegar pelo input
+    if(qtdM == null){
+        return
+    }
+    if(valorMoeda == null){
+        return
+    }
+    var totalBrl = Number.parseFloat(qtdM) * Number.parseFloat(valorMoeda);
+    document.getElementById(total-brl)?.nodeValue = totalBrl.toFixed(2);
+
 }
 
 export default toggleMenu;
